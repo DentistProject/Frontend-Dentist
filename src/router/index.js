3aa2c17d258d85592e4aceed11bfc8d7d2cf890f
jessie <luhui930@126.com>
2023-11-19 20:59:19 +0100

    #2 build up dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DentistDashboard from '../components/DentistDashboard.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,13 @@ const router = createRouter({
         path: '/',
         name: 'home',
         component: HomeView
-      },        
+      },
+      {
+        path: '/dentistdashboard',
+        name: 'DentistDashboard',
+        component: DentistDashboard
+      }, 
+            
     
   ]
 })
