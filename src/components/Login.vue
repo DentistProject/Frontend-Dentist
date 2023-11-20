34010f83674e09b05ff1cf66dd4deb01acbc68ce
jessie <luhui930@126.com>
2023-11-20 22:12:25 +0100

    #6 Implement the log in layout and UI

---
+++ src/components/Login.vue
@@ -0,0 +1,121 @@
+<template>
+<div class="register-photo">
+  <div class="form-container">
+      <div class="image-holder"></div>
+      
+      <form method="post">
+          <h2 class="text-center"><strong>Welcome Dentist System!</strong></h2>
+          <h2 class="text-center"><strong>Log In</strong></h2>
+
+          <div class="form-group"><input class="form-control" type="Username" name="Username" placeholder="Username"></div>
+          <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+          <div class="form-group">
+             
+          </div>
+          <div class="form-group"><button class="btn btn-success btn-block btn-info bt" type="submit">Login</button></div>
+      </form>
+  </div>
+</div>
+  </template>
+  
+  
+  <style>
+  .register-photo {
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+    min-height: 100vh;
+    background: #dfe8f0;
+    padding: 50px 0;
+}
+
+.register-photo form .form-group {
+    margin-bottom: 20px; 
+}
+
+
+.register-photo .image-holder {
+    display: table-cell;
+    width: auto;
+    height: 100%;
+    background: url('../assets/52.png') no-repeat center center fixed;
+   
+}
+
+.register-photo .form-container {
+    display: table;
+    max-width: 1200px;
+    width: 90%;
+    margin: 0 auto;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1)
+}
+
+.register-photo form {
+    display: table-cell;
+    width: 550px;
+    height: 550px;
+    background-color: #ffffff;
+    padding: 60px 60px;
+    color: #505e6c
+}
+
+@media (max-width:991px) {
+    .register-photo form {
+        padding: 40px
+    }
+}
+
+.register-photo form h2 {
+    font-size: 25px;
+    line-height: 1.5;
+    margin-bottom: 30px
+
+}
+
+.register-photo form .form-control {
+    background: transparent;
+    border: none;
+    color:  #416366;
+    border-bottom: 1px solid #dfe7f1;
+    border-radius: 0;
+    box-shadow: none;
+    outline: none;
+    color: inherit;
+    text-indent: 0px;
+    height: 40px
+}
+
+.register-photo form .form-check {
+    font-size: 15px;
+    line-height: 30px
+ 
+}
+
+.register-photo form .btn-primary {
+    background: #61acb3 ;
+    border: none;
+    border-radius: 25px;
+    padding: 25px;
+    box-shadow: none;
+    margin-top: 35px;
+    text-shadow: none;
+    outline: none !important
+}
+
+.register-photo form .btn-primary:hover,
+.register-photo form .btn-primary:active {
+    background:#58b0b8;
+   
+}
+
+.register-photo form .btn-primary:active {
+    transform: translateY(1px);
+    
+}
+.bt{
+  width: 400px;
+}
+
+
+</style>  
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DentistDashboard from '../components/DentistDashboard.vue'
+import Login from '../components/Login.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
         path: '/dentistdashboard',
         name: 'DentistDashboard',
         component: DentistDashboard
+      }, 
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login
       }, 
       
          
