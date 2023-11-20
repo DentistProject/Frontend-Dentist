a05cd538c58e19cf3b5e3b2ed08580d9f47cb4f6
jessie <luhui930@126.com>
2023-11-20 15:58:18 +0100

    #5 update layout

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,94 +1,56 @@
 <template>
-   <form id="contact_form" class="row g-3">
-
-   
-    <!-- Text input-->
-    <div class="mb-3 row">
-      <label class="col-sm-1 col-form-label">Clinic </label>  
-      <div class="col-sm-8">
-        <input name="text" placeholder="Clinic name" class="form-control" type="text">
-      </div>
+  <div class="m-3">
+        <div class="mb-4">
+            <div class="card ">
+                <div class="card-body text-white mailbox-widget pb-0 " style="background-color: #567890">
+                    <h2 class="text-white pb-3   ">Information Setting</h2>
+                </div>
+             
+                
+               
+    <div class="container rounded bg-white mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-3 border-right">
+                <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                    <img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
+                    <span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> 
+                    </span></div>
+            </div>
+            <div class="col-md-5 border-right">
+                <div class="p-3 py-5">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h4 class="text-right">Profile Settings</h4>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
+                        <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
+                        <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value=""></div>
+                        <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
+                        <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
+                        <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
+                        <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div>
+                        <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value=""></div>
+                        <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value=""></div>
+                    </div>
+                    
+                    <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                </div>
+            </div>
+            
+        </div>
     </div>
-    
-    <!-- Text input-->
-    <div class="mb-3 row">
-      <label class="col-sm-1 col-form-label">Mail</label>  
-      <div class="col-sm-8">
-        <input name="email" placeholder="E-Mail Address" class="form-control" type="text">
-      </div>
-    </div>
-    
-    <!-- Text input-->
-    <div class="mb-3 row">
-      <label class="col-sm-1 col-form-label">Phone </label>  
-      <div class="col-sm-8">
-        <input name="phone" placeholder="(845)555-1212" class="form-control" type="text">
-      </div>
-    </div>
-    
-    <!-- Text input-->
-    <div class="mb-3 row">
-      <label class="col-sm-1 col-form-label">Address</label>  
-      <div class="col-sm-8">
-        <input name="address" placeholder="Address" class="form-control" type="text">
-      </div>
-    </div>
-    
-    <!-- Text input-->
-    <div class="mb-3 row">
-      <label class="col-sm-1 col-form-label">City</label>  
-      <div class="col-sm-8">
-        <input name="city" placeholder="City" class="form-control" type="text">
-      </div>
-    </div>
-    
-    <!-- Select Basic -->
-    <div class="mb-3 row"> 
-      <label class="col-sm-1 col-form-label">State</label>
-      <div class="col-sm-8">
-        <select name="state" class="form-select">
-          <option value=" " >Please select your state</option>
-          <!-- Add other options here -->
-        </select>
-      </div>
-    </div>
-
-    <!-- Text input-->
-    <div class="mb-3 row">
-      <label class="col-sm-1 col-form-label">Zip Code</label>  
-      <div class="col-sm-8">
-        <input name="zip" placeholder="Zip Code" class="form-control" type="text">
-      </div>
-    </div>
-    
-   
-    
-   
-    <!-- Text area -->
-    <div class="mb-3 row">
-      <label class="col-sm-1 col-form-label">Description</label>
-      <div class="col-sm-8">
-        <textarea class="form-control" name="comment" placeholder="Clinic Description"></textarea>
-      </div>
-    </div>
-    
- 
-   
-    <!-- Button -->
-    <div class="mb-3 row">
-      <div class="col-sm-10 offset-sm-2">
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </div>
-    </div>
-    
-   
-
-    </form>
+  </div>
+</div>
+</div>  
     </template>
     
-   
-
-
-<style>
-
-</style>
+    <style>
+    .labels {
+        font-size: 11px
+    }
+    
+    
+    </style>
