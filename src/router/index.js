fc81e162323c644290dc18e05eba755f8ca528c5
Joshua Falck <cjoshua@chalmers.se>
2023-12-08 14:15:47 +0100

    #7 add bookings page with cancel abillity

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DentistDashboard from '../components/DentistDashboard.vue'
 import Login from '../components/Login.vue'
-
+import Booking from '../components/Booking.vue'
+import BookingAvailable from '../components/BookingAvailable.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +22,7 @@ const router = createRouter({
         path: '/login',
         name: 'Login',
         component: Login
-      }, 
+      }
       
          
             
