8335616f8c4db07a1eac3d98fb96a31b01980920
Joshua Falck <cjoshua@chalmers.se>
2023-12-11 16:59:22 +0100

    removed unused lines

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DentistDashboard from '../components/DentistDashboard.vue'
 import Login from '../components/Login.vue'
-import Booking from '../components/Booking.vue'
-import BookingAvailable from '../components/BookingAvailable.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
