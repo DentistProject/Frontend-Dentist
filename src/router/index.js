1abb888c65719268a7fc7bb7c37479876491e14a
jessie <luhui930@126.com>
2023-12-11 22:23:38 +0100

    implement login router and Tokn

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DentistDashboard from '../components/DentistDashboard.vue'
 import Login from '../components/Login.vue'
-import Booking from '../components/Booking.vue'
-import BookingAvailable from '../components/BookingAvailable.vue'
+//import Booking from '../components/Booking.vue'
+//import BookingAvailable from '../components/BookingAvailable.vue'
+import { getToken } from '../utils/auth'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,8 @@ const router = createRouter({
       {
         path: '/dentistdashboard',
         name: 'DentistDashboard',
-        component: DentistDashboard
+        component: DentistDashboard,
+        meta: {requiresAuth: true}
       }, 
       {
        
@@ -28,6 +30,31 @@ const router = createRouter({
             
     
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+      // Check if the user is authenticated
+      const token = getToken();
+
+      if (!token) {
+          return next('/'); // Redirect to the login page if not authenticated
+      }
+
+      // Continue to the protected route
+      next();
+  } else {
+      next(); // Allow access to public routes
+  }
+});
+
+function isUserLoggedIn(to, from, next) {
+  if (!getToken()) {
+      next();
+  } else {
+      next('/dentistdashboard');
+  }
+}
+
 
 export default router
